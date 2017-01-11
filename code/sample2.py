1702baec2fcd45384f2600396c78499cefe0d379
佐伯優太 <yuta48691412@gmail.com>
2017-01-11 11:55:05 +0900

    関数名の変更が可能

--- code/sample2.py
+++ code/sample2.py
@@ -3,6 +3,8 @@ from pyparsing import \
    printables, ParseException, restOfLine, alphas, Keyword, \
    SkipTo
 
+import copy
+
 # import pypatt # requires python 2, but did not use
 
 ##################################
@@ -27,26 +29,94 @@ variable = Word(alphas)
 # pattern (not implemented)
 pattern = variable
 # function_name pattern...
-function_name = Group(ZeroOrMore(pattern))
+function_name = ZeroOrMore(pattern)
 # function function_name(){ codes }
-function =  Group(function_name+ lpar + SkipTo('}') + rbrace)
+##function =  Group(function_name + lpar + SkipTo('}') + rbrace)
+
+# argument (pattern...)
+argument = lpar + SkipTo(')') + rpar
+
+# code {...}
+code = lbrace + SkipTo('}') + rbrace
+
+function =  Group(function_name) + Group(argument) + Group(code)
+
 # Layer pattern [ codes ]
-Layer = Group(kLayer + pattern + lbrack + ZeroOrMore(function) + SkipTo(']') + rbrack)
+Layer = Group(kLayer + pattern + lbrack + Group(ZeroOrMore(function)) + SkipTo(']') + rbrack)
+
+##Layer = kLayer + pattern + lbrack + Group(ZeroOrMore(function)) + SkipTo(']') + rbrack
+
 # match ( variable ) { Layer... }
 amatch = ZeroOrMore(Layer)
+
+##amatch = Layer
 
 ##################################
 # Test
 ##result = pat.parseString('long int cal()')
 ##print(result)
 
+
+
+
+# Gen
+def get_name(ast):
+	return ast[-1]
+
+def set_name(ast, layer_name):
+	ast[-1] = ast[-1] + "_" + layer_name
+
+def bind_space(ast):
+	moji = ""
+	for kast in ast:
+		moji += kast + " "
+	return moji
+
+
+def layers(ast, layer_name):
+	set_name(ast[0], layer_name)
+	#function_name = get_name(ast[0])
+	#print function_name
+	line1 = bind_space(ast[0])
+	line2 = '(' +  ast[1][0] + ')'
+	line3 = '{' +  ast[2][0] + '}'
+	print line1
+	print line2
+	print line3
+
+	return line1 + line2
+
+
+def gen(ast):
+	l = []
+	for i, kase in enumerate(ast):
+		if kase[0] == 'Layer':
+			variable = kase[1]
+			#print variable
+			before_func_name = bind_space(kase[2][0])
+			l.append(layers(kase[2], variable))
+		else:
+			print("error")
+
+	print l
+	print before_func_name
+
+
 if __name__ == '__main__':
 	import sys
 	with open(sys.argv[1], 'r') as fp:
 		txt = fp.read()
-		print amatch.parseString(txt)
+		result = amatch.parseString(txt)
+		print result
 
-		
+		print ("-----------------------------")
+
+		gen(result)
+
+
+
+
+
 
 
 
