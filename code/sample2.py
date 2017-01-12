e8f515caf20dd49293b0d8737e93cf5dc9ea8ccf
佐伯優太 <yuta48691412@gmail.com>
2017-01-12 17:42:24 +0900

    if文を作る準備完了

--- code/sample2.py
+++ code/sample2.py
@@ -1,6 +1,6 @@
 from pyparsing import \
    Literal, Word, ZeroOrMore, Group, Dict, Optional, \
-   printables, ParseException, restOfLine, alphas, Keyword, \
+   printables, ParseException, restOfLine, alphas, alphanums, Keyword, \
    SkipTo
 
 import copy
@@ -25,21 +25,25 @@ kLayer = Keyword('Layer')
 # Syntax Rules
 
 # variable (not implemented)
-variable = Word(alphas)
+variable = Word(alphas, alphanums+'_')
 # pattern (not implemented)
 pattern = variable
 # function_name pattern...
+
 function_name = ZeroOrMore(pattern)
 # function function_name(){ codes }
 ##function =  Group(function_name + lpar + SkipTo('}') + rbrace)
 
+#argument_list
+argument_list = function_name + ZeroOrMore(',' + function_name)
+
 # argument (pattern...)
-argument = lpar + SkipTo(')') + rpar
+argument = lpar + argument_list + rpar
 
 # code {...}
 code = lbrace + SkipTo('}') + rbrace
 
-function =  Group(Group(function_name) + argument + code)
+function =  Group(Group(function_name) + Group(argument) + code)
 
 # Layer pattern [ codes ]
 Layer = Group(kLayer + pattern + lbrack + Group(ZeroOrMore(function)) + rbrack)
@@ -61,51 +65,76 @@ amatch = ZeroOrMore(Layer)
 
 # Gen
 def get_func_name(func_ast):
-	return func_ast[0]
+	l = []
+	for i, kase in enumerate(func_ast):
+		if kase == ',':
+			l.append(func_ast[i-1])
+
+	l.append(func_ast[-1])
+	return l
 
 def set_name(ast, layer_name):
 	ast[-1] = ast[-1] + "_" + layer_name
 
 def bind_space(ast):
 	moji = ""
-	for kast in ast:
-		moji += kast + " "
+	for i, kast in enumerate(ast):
+		if i == 0:
+			moji += kast
+		else:
+			moji += ' ' + kast
 	return moji
 
+def bind_comma(ast):
+	moji = ""
+	for i, kast in enumerate(ast):
+		if i == 0:
+			moji += kast
+		else:
+			moji += ',' + kast
+	return moji
 
 
 def gen_func(ast, layer_name):
 	l = []
 	for i, kase in enumerate(ast):
 		before_func_name = bind_space(kase[0])
+		func_name_list = get_func_name(kase[1])
 		set_name(kase[0], layer_name)
 		#function_name = get_name(ast[0])
 		#print function_name
 		line1 = bind_space(kase[0])
-		line2 = '(' +  kase[1] + ')'
+		line2 = '(' +  bind_space(kase[1]) + ')'
 		line3 = '{' +  kase[2] + '}'
 		print line1
 		print line2
 		print line3
-		l.append([line1, line2])
+		l.append([before_func_name, bind_space(kase[1]), kase[0][-1], func_name_list])
 
 	return l
 
 
-def gen_if_sentence(ast, function_name):
+def gen_func_def(ast):
+	mojiretu = ast[0] + '(' + ast[1] + ',' + "int layer_name" + ')'
+	return mojiretu
+
+def gen_if_sentence(ast):
+	l = []
 	for i, kase in enumerate(ast):
-		if kase[0] == 'Layer':
-			variable = kase[1]
-			#print variable
-			before_func_name = bind_space(kase[2][0])
-			l.append(layers(kase[2], variable))
-		else:
-			print("error")
-
+		#print kase
+		line1 = gen_func_def(kase)
+		line2 = "if (layer_name == 0) {return" + kase[2] + '(' + bind_comma(kase[3]) + ');}'
+		print line1
+		print line2
+		l.append([kase[0], line1, line2])
 	print l
-	print before_func_name
-	print ast
+	return l
 
+
+def gen_elif_sentence(ast, katamari):
+	for i, kase in enumerate(ast):
+		#print kase
+		pass
 
 def gen(ast):
 	l = []
@@ -117,8 +146,13 @@ def gen(ast):
 			l.append(gen_func(kase[2], variable))
 		else:
 			print("error")
-	print l
-	#print function_name.parseString(before_func_name)
+	#print l
+
+	for i, kase in enumerate(l):
+		if i == 0:
+			katamari = gen_if_sentence(kase)
+		else:
+			gen_elif_sentence(kase, katamari)
 
 
 if __name__ == '__main__':
