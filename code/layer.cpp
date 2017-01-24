c955aeb4a386505a8464e11c606a9263bc0e7a02
佐伯優太 <yuta48691412@gmail.com>
2017-01-24 19:01:10 +0900

    3章の要点を書き出した

--- code/layer.cpp
+++ code/layer.cpp
@@ -29,15 +29,33 @@ int hoge_outdoor(int name1 , int name2){
                 name = name1 + name2;
                 return name;
         }
+long int cal_middoor(long int val1 , long int val2){
+ long int val;
+                val = val1 - val2;
+                return val;
+        }
+string connect_middoor(string name1 , string name2){
+ string name;
+                name = name2 + name1;
+                return name;
+        }
+int hoge_middoor(int name1 , int name2){
+ int name;
+                name = name1 + name2;
+                return name;
+        }
 long int cal(long int val1 , long int val2,int layer_name){
 if (layer_name == 0) {return cal_indoor(val1,val2);}
 else if (layer_name == 1){return cal_outdoor(val1,val2);}
+else if (layer_name == 2){return cal_middoor(val1,val2);}
 }
 string connect(string name1 , string name2,int layer_name){
 if (layer_name == 0) {return connect_indoor(name1,name2);}
 else if (layer_name == 1){return connect_outdoor(name1,name2);}
+else if (layer_name == 2){return connect_middoor(name1,name2);}
 }
 int hoge(int name1 , int name2,int layer_name){
 if (layer_name == 0) {return hoge_indoor(name1,name2);}
 else if (layer_name == 1){return hoge_outdoor(name1,name2);}
+else if (layer_name == 2){return hoge_middoor(name1,name2);}
 }
