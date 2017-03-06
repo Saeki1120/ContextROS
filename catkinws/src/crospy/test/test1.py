8349fd4137650bfc7e96ee6c3ff5eeebed9a9880
Kenji Hisazumi <kenji@hisazumi.jp>
2017-03-06 15:00:13 +0900

    added CROS testcases

--- catkinws/src/crospy/test/test1.py
+++ catkinws/src/crospy/test/test1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 PKG='test1'
 import unittest
+import rospy
 from crospy import CROS, CPy, cpylayer, cpybase
 
 class CPy1(CPy):
@@ -93,7 +94,76 @@ class CPyTest(unittest.TestCase):
         self.assertEqual(True, obj.base_called)
         self.assertEqual(False, obj.l1_called)
         self.assertEqual(False, obj.l2_called)
+
+class CROS1(CROS):
+    def __init__(self):
+        self.reset()
+        CROS.__init__(self)
+
+    def reset(self):
+        self.base_called = False
+        self.l1_called = False
+        self.l2_called = False
+        
+    @cpybase
+    def test(self):
+        self.base_called = True
+
+    @cpybase
+    def skiptest(self):
+        self.base_called = True
+
+@cpylayer(CROS1, 'l1', 'test')
+def test_1l1(self):
+    self.l1_called = True
+
+@cpylayer(CROS1, 'l2', 'test')
+def test_1l2(self):
+    self.l2_called = True
+    self.proceed()
+
+    
+class CROS2(CROS1):
+    pass
+    
+@cpylayer(CROS2, 'l1', 'test')
+def test_2l1(self):
+    self.l1_called = True
+
+@cpylayer(CROS2, 'l2', 'test')
+def test_2l2(self):
+    self.l2_called = True
+
+    
+class CROSTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(CROSTest, self).__init__(*args, **kwargs)
+        rospy.init_node('test', anonymous=True)
+        self.r = rospy.Rate(0.5)
+
+    def sleep(self):
+        self.r.sleep()
+        
+    def test_base_called(self):
+        c1 = CROS1()
+        c2 = CROS2()
+        c1.test()
+        c2.test()
+        self.assertEqual(True, c1.base_called)
+        self.assertEqual(True, c2.base_called)
+
+    def test_l1_active(self):
+        c1 = CROS1()
+        c2 = CROS2()
+        c1.activate('l1')
+        self.sleep()
+        print(c1._layer)
+        c1.test()
+        c2.test()
+        self.assertEqual(True, c1.l1_called)
+        self.assertEqual(True, c2.l1_called)
         
 if __name__ == '__main__':
     import rosunit
     rosunit.unitrun(PKG, 'test_CPy', CPyTest)
+    rosunit.unitrun(PKG, 'test_CROS', CROSTest)
